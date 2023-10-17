c9d31e04c3633b5a85882537f29c9e6e415a466c
Ousama Oujaber <ndnd15237@gmail.com>
2023-10-16 18:30:54 -0700

    update

--- _printf.c
+++ _printf.c
@@ -2,19 +2,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
- * _printf - Our custom printf function.
- * @format: format string
- * @...: values to format and print
- * Return: number of characters printed
- */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
 	va_list args;
-
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
+	int printed_chars = 0;
+	unsigned int i = 0;
 
 	va_start(args, format);
 
@@ -26,48 +18,35 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 				case 'c':
-					count += write(1, (char[]){(char)va_arg(args, int)}, 1);
-					i += 2;
+					{
+						char c = va_arg(args, int);
+
+						printed_chars += write(1, &c, 1);
+						i++;
+					}
 					break;
 				case 's':
-					count += print_str(va_arg(args, char *));
-					i += 2;
+					{
+						char *str = va_arg(args, char*);
+
+						printed_chars += write(1, str, strlen(str));
+						i++;
+					}
 					break;
 				case '%':
-					count += write(1, &format[i + 1], 1);
-					i += 2;
+					printed_chars += write(1, "%", 1);
+					i++;
 					break;
 			}
 		}
 		else
 		{
-			count += write(1, &format[i], 1);
-			i++;
+			write(1, &format[i], 1);
+			printed_chars++;
 		}
+		i++;
 	}
-
 	va_end(args);
-
-	return (count);
-}
-
-/**
- * print_str - Prints a string.
- * @s: string to print
- * Return: number of characters printed
- */
-int print_str(char *s)
-{
-	int i;
-
-	if (!s)
-		s = "(null)";
-
-	for (i = 0; s[i]; i++)
-		;
-
-	write(1, s, i);
-
-	return (i);
+	return (printed_chars);
 }
 
