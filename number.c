02f8b6cfdf3c4bfb55a9feb9dee4f7f058758c2d
fatimaelasri01 <fatimaelassri041@gmail.com>
2023-10-17 13:38:20 -0700

    number.c

---
+++ number.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * convert - Converts a number to a
+ * string representation in the specified base.
+ * @num: The number to be converted.
+ * @base: The base in which the number should be represented.
+ * @flags: Flags that affect the conversion.
+ * @params: The parameter struct.
+ *
+ * Return: A string representing the number in the specified base.
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+	(void)params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+
+/**
+ * print_unsigned - Prints unsigned integer numbers.
+ * @ap: Argument pointer.
+ * @params: The parameters struct.
+ *
+ * Return: The number of bytes printed.
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+}
+
+
+
+/**
+ * print_address - Prints an address.
+ * @ap: Argument pointer.
+ * @params: The parameters struct.
+ *
+ * Return: The number of bytes printed.
+ */
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
+
